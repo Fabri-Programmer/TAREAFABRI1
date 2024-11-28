ceef10e10efbcc4fc84ef9751ce6942776b1030d
Fabri-Programmer <fabrizziopks@hotmail.com>
2024-11-27 19:51:21 -1100

    Primer guardado

---
+++ SOLUCION/Marihuana.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLUCION
+{
+    public class Marihuana
+    {
+        public int PrecioM { get; set; }
+        public string Calidad { get; set; }
+    }
+}
---
+++ SOLUCION/Tussi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLUCION
+{
+    public class Tussi
+    {
+        public int PrecioT { get; set; }
+        public string Color { get; set; }
+    }
+}
